3b1b30669689079196a1bda5abe17f6cb6a8f5a0
Joshua Wagner <tlrwgnr@gmail.com>
2017-04-08 02:19:09 -0400

    fixed directory name for xcode project

---
+++ buybar-ios/buybar/BarBrain.swift
@@ -0,0 +1,20 @@
+//
+//  BarBrain.swift
+//  buybar
+//
+//  Created by Joshua Wagner on 4/8/17.
+//  Copyright © 2017 Joshua Wagner. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class BarBrain {
+    
+    // gets all available menu items and returns them as an array of Strings
+    func getMenuItems() -> [String] {
+        return [""]
+    }
+    
+    
+}

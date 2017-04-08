76d419d07ef08d1e07e99c9d3b9e344fbcd441f5
Joshua Wagner <tlrwgnr@gmail.com>
2017-04-08 03:00:28 -0400

    set up basic function calls for requests

--- buybar-ios/buybar/BarBrain.swift
+++ buybar-ios/buybar/BarBrain.swift
@@ -10,11 +10,29 @@ import Foundation
 import Alamofire
 
 class BarBrain {
+    var session_id = 0
     
-    // gets all available menu items and returns them as an array of Strings
-    func getMenuItems() -> [String] {
-        return [""]
+    // checks in to bar by starting a session
+    // assigns session id to variable in BarBrain
+    func startSession(sessionID: Int) {
+        self.session_id = sessionID
     }
+    
+    // gets all available menu items and returns them as a dictionary containing [item: price]
+    func getMenuItems() -> [String: String] {
+        return ["": ""]
+    }
+    
+    // sends order to server
+    func sendOrder(itemToOrder: String) {
+        
+    }
+    
+    // closes out session
+    func closeSession(){
+        
+    }
+    
     
     
 }

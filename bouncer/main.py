86f54c5846e4b8368db52a29afd9a462e14664f4
ZackAlfakir <zalfakir@yahoo.com>
2017-04-09 06:16:46 -0400

    flask

--- bouncer/main.py
+++ bouncer/main.py
@@ -48,7 +48,7 @@ def flask():
         print("Posting:")
         client_id="client_58e893bcdaa3258e893bcdaac9"
         secret="secret_edd35b8ec98a8a4fbf9be85c34"
-        r = requests.post("http://ec2-54-236-35-76.compute-1.amazonaws.com/api/v1/sessions", json=(ret), auth=(client_id, secret))
+        r = requests.post("http://ec2-54-236-35-76.compute-1.amazonaws.com/api/v1/sessions", data=(ret), auth=(client_id, secret))
         print("Results:")
         print(r)
         try:

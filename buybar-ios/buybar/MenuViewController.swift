22dcc547e3d3dd42174aa8b53aea220806079734
dkrinke <krinkoid@gmail.com>
2017-04-08 23:07:12 -0400

    Changed the theme of the app. Added some testing for the new logo

---
+++ buybar-ios/buybar/MenuViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MenuViewController.swift
+//  buybar
+//
+//  Created by iostest on 4/8/17.
+//  Copyright © 2017 Joshua Wagner. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    @IBOutlet weak var checkInButton: UIButton!
+    @IBOutlet weak var logo: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.black
+        checkInButton.backgroundColor = UIColor.gray
+        checkInButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+        
+        logo.textColor = UIColor.white
+        
+//        logo.font = logo.font.withSize(80)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
